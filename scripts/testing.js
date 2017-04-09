6c1e5d3c018ae971bb7363dcf2c24c962ad4711d
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-09 16:37:16 -0500

    Wrote excercise functionality using jquery

--- scripts/testing.js
+++ scripts/testing.js
@@ -1,5 +1,43 @@
 $(document).ready(function(){
 
-	console.log("hi");
-	
+	// Simple hello
+	console.log("Hello, World!");
+
+// Excercise No.1
+	// Hides all p tags
+	$("#ex1 button").click(function(){
+		$("#ex1 p").hide();
+	});
+
+// Excercise No.2
+	// Hides a p tag when it is clicked
+	$("#ex2 p").click(function(){
+		$(this).hide();
+	});
+
+// Excercise No.3
+	let numOfAlerts = 1;
+
+	// When moused over alerts once
+	$("#ex3 p").mouseenter(function(){
+		if (numOfAlerts > 0){
+			alert("To hide me double click me!");
+			numOfAlerts -= 1;
+		};
+	});
+
+	// When double clicked hides
+	$("#ex3 p").dblclick(function(){
+		$(this).hide();
+	});
+
+
+
+
+
+
+
+
+
+
 });
